7d88e17e11cbd61b30e1398433f8fb197c0b1679
윤지현 <yunnjhh@gmail.com>
2022-03-12 17:38:33 +0900

    feat: init kafka

---
+++ lecture/SpringForKafka/chap1-clip1/src/main/java/com/fastcampus/springkafka/Ch01/Clip01/Application.java
@@ -0,0 +1,22 @@
+package com.fastcampus.springkafka.Ch01.Clip01;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            kafkaTemplate.send("quickstart-events", "hello !");
+        };
+    }
+}
